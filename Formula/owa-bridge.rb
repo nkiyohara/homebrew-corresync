2b105e2864af41e8c93c37d5c98ae346aff7e45c
Naoki Kiyohara <n.kiyohara23@imperial.ac.uk>
2026-07-20 13:29:07 +0100

    chore: update owa-bridge to 0.3.2

--- Formula/owa-bridge.rb
+++ Formula/owa-bridge.rb
@@ -1,8 +1,8 @@
 class OwaBridge < Formula
   desc "Local-first Outlook Web CLI and MCP server"
   homepage "https://github.com/nkiyohara/owa-bridge"
-  url "https://github.com/nkiyohara/owa-bridge/releases/download/v0.3.1/owa-bridge_0.3.1_source.tar.gz"
-  sha256 "156c51cf0169b99621894a13a8e3c6a081cb8ca0727f96d00a95da9b8a57db6e"
+  url "https://github.com/nkiyohara/owa-bridge/releases/download/v0.3.2/owa-bridge_0.3.2_source.tar.gz"
+  sha256 "b26142f153cdf89fa11fe148eb0604e00b5ac5f44c99f571dd72529a1800f779"
   license "Apache-2.0"
 
   depends_on "go" => :build
@@ -11,8 +11,8 @@ class OwaBridge < Formula
     ldflags = %W[
       -s -w -buildid=
       -X github.com/nkiyohara/owa-bridge/internal/buildinfo.version=#{version}
-      -X github.com/nkiyohara/owa-bridge/internal/buildinfo.commit=2eb65385d70a66dad62a22ea2407a607dd60845d
-      -X github.com/nkiyohara/owa-bridge/internal/buildinfo.buildDate=2026-07-20T12:01:52.264513177Z
+      -X github.com/nkiyohara/owa-bridge/internal/buildinfo.commit=ce23a87e95403fab4117ca7e3a674a0f300fb51e
+      -X github.com/nkiyohara/owa-bridge/internal/buildinfo.buildDate=2026-07-20T12:23:26.639722835Z
     ]
     system "go", "build", "-mod=vendor",
            *std_go_args(output: bin/"owa", ldflags: ldflags.join(" ")),
